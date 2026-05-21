6ec5b8ff1778a305ea07e12e70d57b990140a145
progress <nikita15933@mail.ru>
2026-05-21 09:49:47 +0300

    add choice quality

--- settings_window.py
+++ settings_window.py
@@ -5,13 +5,16 @@ class SettingsWindow(QWidget):
       super().__init__()
 
       # UI Elements
-
       self.choice_prefereded_codec = QComboBox()
       self.choice_prefereded_codec.addItems(["mp3", "aac", "opus", "m4a", "wav", "flac"])
+
+      self.choice_prefereded_quality = QComboBox()
+      self.choice_prefereded_quality.addItems(["128", "192", "256", "320", "0"])
 
       # Layout
       layout = QVBoxLayout()
 
       layout.addWidget(self.choice_prefereded_codec)
-      
+      layout.addWidget(self.choice_prefereded_quality)
+
       self.setLayout(layout)
