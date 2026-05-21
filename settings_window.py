7c045a8508f4940ca90ed24c64473689eb14a7a4
progress <nikita15933@mail.ru>
2026-05-21 09:57:54 +0300

    add save path and button folder

--- settings_window.py
+++ settings_window.py
@@ -1,20 +1,27 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QComboBox
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QComboBox, QLineEdit, QPushButton
 
 class SettingsWindow(QWidget):
    def __init__(self):
       super().__init__()
 
       # UI Elements
-      self.choice_prefereded_codec = QComboBox()
-      self.choice_prefereded_codec.addItems(["mp3", "aac", "opus", "m4a", "wav", "flac"])
+      self.choice_preferred_codec = QComboBox()
+      self.choice_preferred_codec.addItems(["mp3", "aac", "opus", "m4a", "wav", "flac"])
 
-      self.choice_prefereded_quality = QComboBox()
-      self.choice_prefereded_quality.addItems(["128", "192", "256", "320", "0"])
+      self.choice_preferred_quality = QComboBox()
+      self.choice_preferred_quality.addItems(["128", "192", "256", "320", "0"])
+
+      self.save_path = QLineEdit()
+      self.save_path.setPlaceholderText("Путь для сохранения треков (по умолчанию: папка 'downloads' в текущей директории)")
+
+      self.btn_output_folder = QPushButton("Выбрать папку для сохранения")
 
       # Layout
       layout = QVBoxLayout()
 
-      layout.addWidget(self.choice_prefereded_codec)
-      layout.addWidget(self.choice_prefereded_quality)
+      layout.addWidget(self.choice_preferred_codec)
+      layout.addWidget(self.choice_preferred_quality)
+      layout.addWidget(self.save_path)
+      layout.addWidget(self.btn_output_folder)
 
       self.setLayout(layout)
