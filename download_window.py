4aeaca63fb7a005f5aa1d30552ba50ac1253031d
progress <nikita15933@mail.ru>
2026-05-21 09:06:05 +0300

    add download window

---
+++ download_window.py
@@ -0,0 +1,60 @@
+from PySide6.QtWidgets import QMainWindow, QProgressBar, QTabWidget, QTextEdit, QVBoxLayout, QPushButton, QWidget
+from download_thread import DownloadThread
+from config import Config
+import os
+
+class DownloadWindow(QWidget):
+   def __init__(self):
+      super().__init__()
+      self.setWindowTitle("Скачивание треков")
+
+      # UI Elements
+      self.tabs = QTabWidget()
+
+      self.url_input = QTextEdit()
+      self.url_input.setPlaceholderText("Вставьте ссылки на YouTube (через пробел, запятую или построчно).")
+
+      self.btn_download = QPushButton("Скачать все")
+
+      self.progress_bar = QProgressBar()
+
+      self.log_message = QTextEdit()
+      self.log_message.setReadOnly(True)
+
+      # Layout
+      layout = QVBoxLayout()
+
+      layout.addWidget(self.url_input)
+      layout.addWidget(self.btn_download)
+      layout.addWidget(self.progress_bar)
+      layout.addWidget(self.log_message)
+
+      self.container = QWidget()
+
+      self.container.setLayout(layout)
+      self.btn_download.clicked.connect(self.start_download)
+
+      self.config = Config()
+      self.create_output_folder(self.config.config.get("output", "downloads"))
+
+   def create_output_folder(self, output_folder):
+      if not os.path.exists(output_folder):
+         os.makedirs(output_folder)
+
+   def start_download(self):
+      urls = self.url_input.toPlainText().strip()
+      if not urls:
+         return
+
+      url_list = [url.strip() for url in urls.replace(",", " ").split() if url.strip()]
+      self.download_thread = DownloadThread(url_list, self.config)
+      self.download_thread.progress.connect(self.update_progress)
+      self.download_thread.finished.connect(self.download_finished)
+      self.download_thread.log_message.connect(self.log_message.append)
+      self.download_thread.start()
+
+   def update_progress(self, value):
+      self.progress_bar.setValue(value)
+
+   def download_finished(self):
+      self.progress_bar.setValue(100)
