1f2c7299631110754481600d2ea67c9c50b61c35
progress <nikita15933@mail.ru>
2026-05-22 09:40:10 +0300

    add save and property

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
+import json
+
+
 class Config:
-   def __init__(self, path_config="config.json", yt_dlp_config="yt-dlp-config.json"):
+   def __init__(self, path_config="config.json"):
       self.path_config = path_config
-      self.yt_dlp_config = yt_dlp_config
       self.config = self.load_config()
-      self.yt_dlp_config_data = self.load_yt_dlp_config()
 
    def load_config(self):
-      import json
       with open(self.path_config, 'r') as f:
          return json.load(f)
-
-   def load_yt_dlp_config(self):
-      import json
-      with open(self.yt_dlp_config, 'r') as f:
-         return json.load(f)
    
+   def save_config(self):
+      with open(self.path_config, 'w', encoding='utf-8') as f:
+         json.dump(self.config, f, indent=3, ensure_ascii=False)
+   
+   @property
+   def yt_dlp_data(self):
+      return self.config.get("yt-dlp-config", {})
